7bd6dd9a78a77d22edeb242907821a382c474bfa
parkerroy <69988242+parkerroy@users.noreply.github.com>
2021-04-15 18:57:00 -0500

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,29 @@
+  
+import pandas as pd
+import boto3
+import io
+from io import StringIO
+
+def lambda_handler(event, context):
+    s3_file_key = event['Records'][0]['s3']['object']['key'];
+    bucket = '<s3 bucket name>';
+    s3 = boto3.client('s3', aws_access_key_id='<aws access key>',  aws_secret_access_key='<aws secret key>')
+    obj = s3.get_object(Bucket=bucket, Key=s3_file_key)
+    initial_df = pd.read_csv(io.BytesIO(obj['Body'].read()));
+
+    service_name = 's3'
+    region_name = 'us-east-2'
+    aws_access_key_id = '<aws access key>'
+    aws_secret_access_key = '<aws secret key>'
+
+    s3_resource = boto3.resource(
+        service_name=service_name,
+        region_name=region_name,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+    bucket='parkerroy-destination';
+    df = initial_df[(initial_df.species == "setosa")];
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer,index=False);
+    s3_resource.Object(bucket, s3_file_key).put(Body=csv_buffer.getvalue())
